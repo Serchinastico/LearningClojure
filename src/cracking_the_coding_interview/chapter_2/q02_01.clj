9e8dc094bd0e9c44274ed79b539af66935aa534b
Sergio Gutierrez <sergio@gokarumi.com>
2016-04-02 21:05:22 +0200

    Solution to problem 1 with clojure lists

--- src/cracking_the_coding_interview/chapter_2/q02_01.clj
+++ src/cracking_the_coding_interview/chapter_2/q02_01.clj
@@ -6,11 +6,10 @@
 ; element is already in the list, we remove it by pointing its previous node
 ; to the next one.
 ;
-; Complexity: O(n) where n = (count string)
-;   Assuming: first, rest & conj are O(1)
+; Complexity: O(n) where n = (count ll)
 
 (defn remove-duplicates-v1
-  "Remove all duplicated elements from a list"
+  "Remove all duplicated elements from a linked list"
   [ll]
   (defn remove-duplicate-nodes
     [node found-elements]
@@ -23,3 +22,38 @@
   (ll-unitary (remove-duplicate-nodes (ll-first ll) #{})))
 
 (ll-to-seq (remove-duplicates-v1 (to-ll "hola")))
+
+; Version 2: Same solution as in version 1 but using clojure a list.
+;
+; Complexity: O(n) where n = (count list)
+
+(defn remove-duplicates-v2
+  "Remove all duplicated elements from a clojure list"
+  [list]
+  (reverse
+    (loop [in-list list
+           out-list '()
+           found-elements #{}]
+      (if (empty? in-list)
+        out-list
+        (if (contains? found-elements (first in-list))
+          (recur
+            (rest in-list)
+            out-list
+            found-elements)
+          (recur
+            (rest in-list)
+            (conj out-list (first in-list))
+            (conj found-elements (first in-list))))))))
+
+; Version 3: Same solution as in version 2 but using the reduce function.
+;
+; Complexity: O(n) where n = (count list)
+(defn remove-duplicates-v3
+  [list]
+  (reverse (second (reduce (fn [[found-elements out] element]
+                             (if (contains? found-elements element)
+                               [found-elements out]
+                               [(conj found-elements element) (conj out element)]))
+                           [#{} '()]
+                           list))))
--- test/cracking_the_coding_interview/chapter_2/q02_01_test.clj
+++ test/cracking_the_coding_interview/chapter_2/q02_01_test.clj
@@ -7,7 +7,7 @@
   [in]
   (apply str (ll-to-seq (remove-duplicates-v1 (to-ll in)))))
 
-(deftest remove-duplicates-test
+(deftest remove-duplicates-v1-test
   (is (= (remove-duplicates-str-v1 "") ""))
   (is (= (remove-duplicates-str-v1 "a") "a"))
   (is (= (remove-duplicates-str-v1 "aa") "a"))
@@ -15,3 +15,21 @@
   (is (= (remove-duplicates-str-v1 "abc") "abc"))
   (is (= (remove-duplicates-str-v1 "abcabc") "abc"))
   (is (= (remove-duplicates-str-v1 "abccccccc") "abc")))
+
+(deftest remove-duplicates-v2-test
+  (is (= (remove-duplicates-v2 "") '()))
+  (is (= (remove-duplicates-v2 "a") '(\a)))
+  (is (= (remove-duplicates-v2 "aa") '(\a)))
+  (is (= (remove-duplicates-v2 "aaa") '(\a)))
+  (is (= (remove-duplicates-v2 "abc") '(\a, \b, \c)))
+  (is (= (remove-duplicates-v2 "abcabc") '(\a, \b, \c)))
+  (is (= (remove-duplicates-v2 "abccccccc") '(\a, \b, \c))))
+
+(deftest remove-duplicates-v3-test
+  (is (= (remove-duplicates-v3 "") '()))
+  (is (= (remove-duplicates-v3 "a") '(\a)))
+  (is (= (remove-duplicates-v3 "aa") '(\a)))
+  (is (= (remove-duplicates-v3 "aaa") '(\a)))
+  (is (= (remove-duplicates-v3 "abc") '(\a, \b, \c)))
+  (is (= (remove-duplicates-v3 "abcabc") '(\a, \b, \c)))
+  (is (= (remove-duplicates-v3 "abccccccc") '(\a, \b, \c))))
